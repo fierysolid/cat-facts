a35c67d0503f69bfa7b3751df0f4adc3317c805f
Jordan Schoen <schoen.jordan@gmail.com>
2017-07-27 16:15:30 -0400

    cat facts complete

--- app.js
+++ app.js
@@ -1,84 +1,47 @@
-// Load app dependencies
-var http = require('http'),
-    path = require('path'),
-    express = require('express'),
-    twilio = require('twilio');
+'use strict';
 
-// Load configuration information from system environment variables.
-var TWILIO_ACCOUNT_SID = process.env.TWILIO_ACCOUNT_SID,
+require('dotenv').config({
+    silent: true
+});
+
+var request = require('request'),
+    twilio = require('twilio'),
+    TWILIO_ACCOUNT_SID = process.env.TWILIO_ACCOUNT_SID,
     TWILIO_AUTH_TOKEN = process.env.TWILIO_AUTH_TOKEN,
-    TWILIO_NUMBER = process.env.TWILIO_NUMBER;
+    TWILIO_NUMBER = process.env.TWILIO_NUMBER,
+    client = twilio(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN),
+    requestUrl,
+    count = 0,
+    bodyText;
 
-// Create an authenticated client to access the Twilio REST API
-var client = twilio(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN);
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min;
+}
 
-// Create an Express web application with some basic configuration
-var app = express();
-
-app.configure(function(){
-    app.set('port', process.env.PORT || 3000);
-    app.set('views', __dirname + '/views');
-    app.set('view engine', 'ejs');
-    app.use(express.favicon());
-    app.use(express.logger('dev'));
-    app.use(express.bodyParser());
-    app.use(express.methodOverride());
-    app.use(app.router);
-    app.use(express.static(path.join(__dirname, 'public')));
-});
-
-app.configure('development', function(){
-    app.use(express.errorHandler());
-});
-
-// render our home page
-app.get('/', function(request, response) {
-    response.render('index');
-});
-
-// handle a POST request to send a text message.  This is sent via ajax on our
-// home page
-app.post('/message', function(request, response) {
-    // Use the REST client to send a text message
-    client.sendSms({
-        to: request.param('to'),
-        from: TWILIO_NUMBER,
-        body: 'Good luck on your Twilio quest!'
-    }, function(err, data) {
-        // When we get a response from Twilio, respond to the HTTP POST request
-        response.send('Message is inbound!');
+setInterval(function() {
+    requestUrl = 'http://www.catfact.info/api/v1/facts.json?page=' + getRandomInt(1, 676) + '&per_page=1';
+    request({
+        url: requestUrl
+    }, function(error, response, body) {
+        body = JSON.parse(body);
+        if (!error) {
+            if (count === 1) {
+                bodyText = 'Text STOP to end cat facts.'
+            } else {
+                bodyText = body.facts[0].details
+            }
+            client.sendSms({
+                to: '+12088906912',
+                from: TWILIO_NUMBER,
+                body: bodyText
+            }, function(err, data) {
+                if (err) console.log('Error: ', err);
+                console.log('Data: ', data);
+            });
+            count++;
+            if (count === 3) count = 0;
+        }
     });
-});
-
-// handle a POST request to make an outbound call.  This is sent via ajax on our
-// home page
-app.post('/call', function(request, response) {
-    // Use the REST client to send a text message
-    client.makeCall({
-        to: request.param('to'),
-        from: TWILIO_NUMBER,
-        url: 'http://twimlets.com/message?Message%5B0%5D=http://demo.kevinwhinnery.com/audio/zelda.mp3'
-    }, function(err, data) {
-        // When we get a response from Twilio, respond to the HTTP POST request
-        response.send('Call incoming!');
-    });
-});
-
-// Create a TwiML document to provide instructions for an outbound call
-app.get('/hello', function(request, response) {
-    // Create a TwiML generator
-    var twiml = new twilio.TwimlResponse();
-    twiml.say('Hello there! You have successfully configured a web hook.');
-    twiml.say('Good luck on your Twilio quest!', { 
-        voice:'woman' 
-    });
-
-    // Return an XML response to this request
-    response.set('Content-Type','text/xml');
-    response.send(twiml.toString());
-});
-
-// Start our express app, by default on port 3000
-http.createServer(app).listen(app.get('port'), function(){
-    console.log("Express server listening on port " + app.get('port'));
-});
+}, 100000); // 10 minutes
