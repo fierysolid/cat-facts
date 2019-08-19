8401585550d9f7e1b86786789dbdc7d9dc2e5f80
Jordan Schoen <schoen.jordan@gmail.com>
2019-08-19 15:33:58 -0400

    upgrades

--- app.js
+++ app.js
@@ -1,69 +1,72 @@
-'use strict';
+"use strict";
 
-require('dotenv').config({
-    silent: true
+require("dotenv").config({
+  silent: true
 });
 
-var request = require('request'),
-    twilio = require('twilio'),
-    TWILIO_ACCOUNT_SID = process.env.TWILIO_ACCOUNT_SID,
-    TWILIO_AUTH_TOKEN = process.env.TWILIO_AUTH_TOKEN,
-    TWILIO_NUMBER = process.env.TWILIO_NUMBER,
-    client = twilio(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN),
-    requestUrl,
-    init = true,
-    count = 0,
-    number = '+15035025329'; //'+1208'
-
-function getRandomInt(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min)) + min;
-}
+var request = require("request-promise"),
+  client = require("twilio")(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN),
+  TWILIO_ACCOUNT_SID = process.env.TWILIO_ACCOUNT_SID,
+  TWILIO_AUTH_TOKEN = process.env.TWILIO_AUTH_TOKEN,
+  TWILIO_NUMBER = process.env.TWILIO_NUMBER,
+  requestUrl,
+  init = true,
+  count = 0,
+  number = "‭+12087616324‬", //'+1208'
+  requestUrl = "https://catfact.ninja/fact";
 
 function sendMessages() {
-    if (count === 0) {
-        if (init) {
-            client.sendSms({
-                to: number,
-                from: TWILIO_NUMBER,
-                body: 'Thanks for signing up for Cat Facts! You now will receive fun daily facts about CATS! >o<, to cancel Daily Cat Facts, reply \'SEXUAL CHOCOLATE\''
-            }, function(err, data) {
-                if (err) console.log('Error: ', err.message);
-                console.log('Text: ', data.body);
-            });
-            init = false;
-        } else {
-            client.sendSms({
-                to: number,
-                from: TWILIO_NUMBER,
-                body: 'To cancel Daily Cat Facts, reply \'SEXUAL CHOCOLATE\''
-            }, function(err, data) {
-                if (err) console.log('Error: ', err.message);
-                console.log('Text: ', data.body);
-            });
-        }
-        count++;
+  /////// Send Single Message ////////
+  //   client.messages
+  //     .create({
+  //       to: number,
+  //       from: TWILIO_NUMBER,
+  //       body: "ANSWER MY FUCKING QUESTIONS TY"
+  //     })
+  //     .then(message => console.log("Text: ", message.body))
+  //     .catch(err => console.log("Error: ", err.message));
+
+  /////// Send Cat Facts ////////
+  if (count === 0) {
+    if (init) {
+      client.messages
+        .create({
+          to: number,
+          from: TWILIO_NUMBER,
+          body:
+            "Thanks for signing up for Cat Facts! You now will receive fun daily facts about CATS! >o<, to cancel Daily Cat Facts, reply 'SEXUAL CHOCOLATE'"
+        })
+        .then(data => console.log("Text: ", data.body))
+        .catch(err => console.log("Error: ", err.message));
+      init = false;
     } else {
-        requestUrl = 'https://catfact.ninja/fact';
-        request({
-            url: requestUrl
-        }, function(error, response, body) {
-            body = JSON.parse(body);
-            if (!error) {
-                client.sendSms({
-                    to: number,
-                    from: TWILIO_NUMBER,
-                    body: body.fact
-                }, function(err, data) {
-                    if (err) console.log('Error: ', err.message);
-                    console.log('Text: ', data.body);
-                });
-                count++;
-                if (count === 10) count = 0;
-            }
-        });
+      client.messages
+        .create({
+          to: number,
+          from: TWILIO_NUMBER,
+          body: "To cancel Daily Cat Facts, reply 'SEXUAL CHOCOLATE'"
+        })
+        .then(data => console.log("Text: ", data.body))
+        .catch(err => console.log("Error: ", err.message));
     }
+    count++;
+  } else {
+    request({ uri: requestUrl, json: true }).then((error, response, body) => {
+      body = JSON.parse(body);
+      if (!error) {
+        client.messages
+          .create({
+            to: number,
+            from: TWILIO_NUMBER,
+            body: body.fact
+          })
+          .then(data => console.log("Text: ", data.body))
+          .catch(err => console.log("Error: ", err.message));
+        count++;
+        if (count === 10) count = 0;
+      }
+    });
+  }
 }
 
 sendMessages();
